7378a56be3ec5c479a8fcab78884d70dfa20ef79
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-02-22 14:49:53 -0800

    finished schema and populated mongo with node

---
+++ database/dataGenerator.js
@@ -0,0 +1,102 @@
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+// //data capsule
+// let data = [];
+
+//create ID counter
+let idCount = 1;
+
+//longitude generator
+let longitude = () => {
+  let randomData = Math.random() * (90 - (-90) + 1) + -90;
+  return randomData.toFixed(6).toString();
+};
+
+//latitude generator
+let latitude = () => {
+  let randomData = Math.random() * (180 - (-180) + 1) + -180;
+  return randomData.toFixed(6).toString();
+};
+
+//phone number generator 
+let phoneNumber = () => {
+  let areaCode = Math.floor(Math.random() * (999 - 100 + 1)) + 100;
+  let threeDigits = Math.floor(Math.random() * (999 - 100 + 1)) + 100;
+  let lastDigits = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
+  return `+1(${areaCode})${threeDigits}-${lastDigits}`;
+};
+
+//price range generator
+let priceRange = () => {
+  let min = Math.floor(Math.random() * (50 - 2 + 1)) + 2;
+  let max = Math.floor(Math.random() * (1000 - 51 + 1)) + 51;
+  return `$${min} - $${max}`;
+};
+
+//function to create mock data. Will make 200 instances of the same schema and insert it into the database
+//generate the actual data
+let generateMockData = () => {
+  //data capsule
+  let data = [];
+  for (let i = 0; i < 200; i++) {
+    data.push({restaurant_id: idCount,
+       name: faker.company.companyName(),
+       latitude: latitude(),
+       longitude: longitude(),
+       diningStyle: faker.lorem.word(),
+       cuisines: faker.lorem.word(),
+       hoursOfOperations: {
+       breakfast: {
+         served : true,
+         times: faker.lorem.words(),
+         days: faker.lorem.words()
+       },
+       brunch: {
+         Served: true, 
+         times: faker.lorem.words(),
+         days: faker.lorem.words()
+       },
+       lunch: {
+         served : true,
+         times: faker.lorem.words(),
+         days: faker.lorem.words()
+       },
+       dinner: {
+         served : true,
+         times: faker.lorem.words(),
+         days: faker.lorem.words()
+       } 
+      }, 
+      crossStreet: faker.lorem.sentence(),
+      dressCode: faker.lorem.sentence(),
+      priceRange: priceRange(),
+      paymentOptions: {
+        visa: true,
+        master: true,
+        amex: true,
+        discover: true
+      },
+      phoneNumber: phoneNumber() ,
+      website: `${faker.lorem.word()}.com`,
+      catering: {
+        cater: true,
+        description: faker.lorem.paragraph()
+      },
+      publicTransit: faker.lorem.sentence(),
+      executiveChef: faker.lorem.paragraph(),
+      additional: {
+        chef: true, 
+        description: faker.lorem.paragraph()
+      },
+      specialEvents: faker.lorem.paragraph(), 
+      promotions: faker.lorem.paragraph()
+    });
+    idCount++;
+  }
+  console.log(data);
+  return data;
+};
+
+
+module.exports.generateMockData = generateMockData;
--- database/index.js
+++ database/index.js
@@ -1,18 +1,144 @@
 const mongoose = require('mongoose');
+const faker = require('faker');  //faker will be used to generate dummy data
+const dataGenerator = require('./dataGenerator.js');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/restaurants', {useMongoClient: true});
-
+mongoose.connect('mongodb://localhost/restaurants');
+// , {useMongoClient: true}
 //defining the location schema and data shape
 let locationSchema = mongoose.Schema({
-
+  restaurant_id: Number,
+  name: String,
+  latitude: String,
+  longitude: String
 });
 
-let location = mongoose.model('location', locationSchema);
+let Location = mongoose.model('location', locationSchema);
 
-
-//defining the information schema and data shape 
 let informationSchema = mongoose.Schema({
-
+  restaurant_id: Number,
+  name: String,
+  diningStyle: String,
+  cuisines: String,
+  hoursOfOperations: {
+   breakfast: {
+     served : Boolean,
+     times: String,
+     days: String
+   },
+   brunch: {
+     Served: Boolean, 
+     times: String,
+     days: String
+   },
+   lunch: {
+     served : Boolean,
+     times: String,
+     days: String
+   },
+   dinner: {
+     served : Boolean,
+     times: String,
+     days: String
+   } 
+  }, 
+  crossStreet: String,
+  dressCode: String,
+  priceRange: String,
+  paymentOptions: Object,
+  phoneNumber: String,
+  website: String,
+  catering: {
+    cater: Boolean,
+    description: String
+  },
+  publicTransit: String,
+  executiveChef: String,
+  additional: {
+    chef: Boolean, 
+    description: String
+  },
+  specialEvents: String, 
+  promotions: String
 });
 
-let information = mongoose.model('information', informationSchema);
+let Information = mongoose.model('Information', informationSchema);
+
+//variable which will have the data in an array object with nested objects
+let data = dataGenerator.generateMockData();
+console.log(data);
+
+let save = (data) => {
+  data.forEach((item) => {
+    let restaurantLocation = new Location({
+      id: item.restaurant_id,
+      name: item.name,
+      latitude: item.latitude,
+      longitude: item.longitude
+    });
+
+    let restaurant = new Information({
+      restaurant_id: item.restaurant_id,
+       name: item.name,
+       diningStyle: item.diningStyle,
+       cuisines: item.cuisines,
+       hoursOfOperations: {
+        breakfast: {
+          served : item.hoursOfOperations.breakfast.served,
+          times: item.hoursOfOperations.breakfast.times,
+          days: item.hoursOfOperations.breakfast.days
+        },
+        brunch: {
+          served: item.hoursOfOperations.brunch.served, 
+          times: item.hoursOfOperations.brunch.times,
+          days: item.hoursOfOperations.brunch.days
+        },
+        lunch: {
+          served : item.hoursOfOperations.lunch.served,
+          times: item.hoursOfOperations.lunch.times,
+          days: item.hoursOfOperations.lunch.days
+        },
+        dinner: {
+          served : item.hoursOfOperations.dinner.served,
+          times: item.hoursOfOperations.dinner.times,
+          days: item.hoursOfOperations.dinner.days
+        } 
+      }, 
+      crossStreet: item.crossStreet,
+      dressCode: item.dressCode,
+      priceRange: item.priceRange,
+      paymentOptions: {
+        visa: item.paymentOptions.visa,
+        master: item.paymentOptions.master,
+        amex: item.paymentOptions.amex,
+        discover: item.paymentOptions.discover
+      },
+      phoneNumber: item.phoneNumber,
+      website: item.website,
+      catering: {
+        cater: item.catering.cater,
+        description: item.catering.description
+      },
+      publicTransit: item.publicTransit,
+      executiveChef: item.executiveChef,
+      additional: {
+        chef: item.additional.chef, 
+        description: item.additional.description
+      },
+      specialEvents: item.specialEvents, 
+      promotions: item.promotions
+    });
+
+    restaurantLocation.save((err) => {
+      if (err) return handleError(err);
+    });
+
+    restaurant.save((err) => {
+      if (err) return handleError(err);
+    });
+  });
+}
+
+//invocation of the save function to populate the database
+save(data);
+
+module.exports.save = save;
