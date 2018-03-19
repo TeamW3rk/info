a208b9d9b6482b7ffab0b74f0c45c03a6207c4ba
Melvin Estrada <melvinlouie.estrada@gmail.com>
2018-03-19 10:23:08 -0700

    removed duplicate files

--- database/index.js
+++ database/index.js
@@ -1,56 +1,95 @@
-const faker = require('faker');  //faker will be used to generate dummy data
+const pgp = require('pg-promise')({});
+const path = require('path');
 const dataGenerator = require('./dataGenerator.js');
-const router = require('../server/router.js');
 
-let informationSchema = mongoose.Schema({
-  restaurant_id: Number,
-  name: String,
-  latitude: String,
-  longitude: String,
-  map: String, 
-  diningStyle: String,
-  cuisines: String,
-  hoursOfOperations: {
-   monday: {
-     served : Boolean,
-     lunch: String,
-     dinner: String
-   },
-   friday: {
-     served: Boolean, 
-     lunch: String,
-     dinner: String
-   },
-   saturday: {
-     served : Boolean,
-     lunch: String,
-     dinner: String
-   }
-  }, 
-  crossStreet: String,
-  dressCode: String,
-  priceRange: String,
-  paymentOptions: Object,
-  phoneNumber: String,
-  website: String,
-  catering: {
-    cater: Boolean,
-    description: String
-  },
-  publicTransit: String,
-  executiveChef: String,
-  additional: {
-    chef: Boolean, 
-    description: String
-  },
-  specialEvents: String, 
-  promotions: String,
-  rating: String,
-  reviews: String,
-  topTags: Array,
-  description: String,
-  neighborhood: String,
-  parking: String
-});
+const QueryFile = pgp.QueryFile;
 
-module.exports.information = information;
+const sql = (file) => {
+  const filePath = path.join(__dirname, file);
+  console.log(filePath)
+  return new QueryFile(filePath, { minify: true });
+};
+
+const schema = sql('/create_schema.sql');
+const table = sql('/create_table.sql');
+
+const columnSet = [
+  'restaurant_id',
+  'name',
+  'latitude',
+  'longitude',
+  'map',
+  'dining_style',
+  'cuisines',
+  'hours_of_operations',
+  'cross_street',
+  'dress_code',
+  'price_range',
+  'payment_options',
+  'phone_number',
+  'website',
+  'catering',
+  'public_transit',
+  'executive_chef',
+  'additional',
+  'special_events',
+  'promotions',
+  'rating',
+  'reviews',
+  'top_tags',
+  'description',
+  'neighborhood',
+  'parking',
+];
+
+const tableName = { table: 'info'};
+
+const createTable = async() => {
+  let db = pgp({
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres',
+    user: 'mcestrada2',
+  });
+  await db.none(schema);
+  db.$pool.end;
+  db = pgp({
+    host: 'localhost',
+    port: 5432,
+    database: 'restaurants',
+    user: 'mcestrada2',
+  });
+  return db.none(table).then(() => db);
+}
+
+function getNextData(t, pageIndex) {
+  let data = null;
+  if (pageIndex < 10000) {
+    if(pageIndex % 100 === 0) {console.log(pageIndex * 1000, (new Date - startTime) / 60000)};
+    data = [];
+    for (let i = 0; i < 1000; i++) {
+      const idx = pageIndex * 1000 + i; // to insert unique product names
+      data.push(dataGenerator.generateSingleData(idx));
+    }
+  }
+  return Promise.resolve(data);
+}
+const startTime = new Date();
+const run = async () => {
+  const db = await createTable();
+  const cs = new pgp.helpers.ColumnSet(columnSet, tableName);
+  const response = await db.tx('massive-insert', t => {
+    return t.sequence(index => {
+      return getNextData(t, index)
+        .then(data => {
+          if(data) {
+            const insert = pgp.helpers.insert(data, cs);
+            return t.none(insert);
+          }
+        });
+    });
+  });
+  console.log('Total batches:', response.total, ', Duration:', response.duration / 60000);
+  db.$pool.end;
+}
+run();
